37a1b88176762da407fb6361aafeb422317da60a
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-05-31 22:20:31 +0900

    fix: login API 구현.

---
+++ onlineshop/src/main/java/com/example/onlineshop/dtos/LoginRequestDto.java
@@ -0,0 +1,7 @@
+package com.example.onlineshop.dtos;
+
+public record LoginRequestDto(
+    String email,
+    String password
+) {
+}
---
+++ onlineshop/src/main/java/com/example/onlineshop/dtos/LoginResultDto.java
@@ -0,0 +1,7 @@
+package com.example.onlineshop.dtos;
+
+public record LoginResultDto(
+    String accessToken
+) {
+
+}
